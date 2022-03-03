91c4483c92748ea6c4f5e0f046cc795f1babb4fd
therealbeez <gabriel.agbese2@gmail.com>
2022-03-03 13:23:27 +0100

    initial functional definition of card componenets

--- src/components/cardcontainer.js
+++ src/components/cardcontainer.js
@@ -12,11 +12,17 @@ export default function CardContainer(params) {
         <div className="card-container">
             
             <div class="card" id="card-3" >
-                <div className="head" onClick={() => {cardThreeSelect()}}>card3</div>
-                <a  className="close" onClick={() => {cardThreeClose()}}>xbutton</a>
+                <div className="head" onClick={() => {cardSelect(3)}}>card3</div>
+                <a  className="close" onClick={() => {cardSelect(3)}}>x button</a>
             </div>
-            <div class="card" id="card-2">Card2</div>
-            <div class="card" id="card-1">Card1</div>
+            <div class="card" id="card-2">
+                <div className="head" onClick={() => {cardSelect(2)}}>card2</div>
+                <a  className="close" onClick={() => {cardSelect(2)}}>x button</a>
+            </div>
+            <div class="card" id="card-1">
+                <div className="head" onClick={() => {cardSelect(1)}}>card1</div>
+                <a  className="close" onClick={() => {cardSelect(1)}}>x button</a>
+            </div>
         </div>
     )
 }
@@ -24,20 +30,39 @@ export default function CardContainer(params) {
 //animation for the cards
 var num = -25;
 var pos = num+'vh'
-const tl = gsap.timeline();
+var tl1 = new gsap.timeline();
+var tl2 = new gsap.timeline();
+var tl3 = new gsap.timeline();
 
-const ani = () => {
-    tl.to("#card-3",{ 
-        y: pos,
-    }); 
-    return tl
+const ani = (a) => {
+    const id = "#card-"+ a;
+    switch (a) {
+        case 1:
+            tl1.to(id, { 
+                y: pos,
+            }); 
+            return tl1
+        case 2: 
+            tl2.to(id, { 
+                y: pos,
+            }); 
+            return tl2
+        case 3:
+            case 2: 
+            tl3.to(id, { 
+                y: pos,
+            }); 
+            return tl3
+        default:
+            break;
+    }
 };
 
-//card 3
-function cardThreeSelect(){
-    ani().play()
-}
 
-function cardThreeClose(){
-    ani().reverse()
+let isActivated = false;
+
+function cardSelect(b){
+    isActivated ? ani(b).reverse() : ani(b).play();
+    isActivated = !isActivated;
+    
 }
