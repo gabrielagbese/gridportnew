75d9aa14a1d231354e58c2d46708fe205dfe5b1b
therealbeez <gabriel.agbese2@gmail.com>
2022-03-04 19:03:37 +0100

    made the card tweens responsive

--- src/components/cardcontainer.js
+++ src/components/cardcontainer.js
@@ -3,10 +3,8 @@ import "./styles/style.css"
 import { gsap } from "gsap";
 
 
-
 export default function CardContainer(params) {
 
-    
 
     return(
         <div className="card-container">
@@ -38,59 +36,64 @@ var tl3 =  gsap.timeline();
 
 //card animation instructions
 const ani = (a) => {
-    if(isActivated == true){
-        reverseTimelines()
-    }else{
-        const id = "#card-"+ a;
+
+    if(window.innerWidth >= 720){
+        //desktop screens
         switch (a) {
             case 1:
-                tl1.to(id, { y: -60+'vh',}); 
+                tl1.to("#card-1", { y: -60+'vh',}).timeScale(1.0); 
                 return tl1
             case 2: 
-                tl2.to(id, { 
+                tl2.to("#card-2", { 
                     y: -50+'vh',
                 });
-                tl2.to("#card-1", {y: 10+'vh',})
+                tl2.to("#card-1", {y: 10+'vh',}).timeScale(1.0)
                 return tl2
             case 3:
                 tl3.to("#card-2", { 
                     y:10+'vh',
                 });
-                tl3.to("#card-1", {y: 10+'vh',})
-                tl3.to(id, { 
+                tl3.to("#card-1", {y: 10+'vh',}).timeScale(1.0)
+                tl3.to("#card-3", { 
                     y: -20+'vh',
                 });
                 return tl3
             default:
                 break;
         }
+
+    }else{
+        //mobile screens
+        switch (a) {
+            case 1:
+                tl1.to("#card-1", { y: -70+'vh',}).timeScale(1.0); 
+                return tl1
+            case 2: 
+                tl2.to("#card-2", { 
+                    y: -60+'vh',
+                });
+                tl2.to("#card-1", {y: 10+'vh',}).timeScale(1.0)
+                return tl2
+            case 3:
+                tl3.to("#card-2", { y:10+'vh',});
+                tl3.to("#card-1", {y: 10+'vh',}).timeScale(1.0)
+                tl3.to("#card-3", { y: -40+'vh',});
+                return tl3
+            default:
+                break;
+        }
+
     }
 };
 
 //reversal of card activation animation 
-function reverseTimelines(tl) {
-    switch (tl) {
-        case 1:
-            tl1.reverse()
-            console.log(tl+" reversed")
-            break;
-        case 2:
-            tl2.reverse()
-            console.log(tl+" reversed")
-            break;
-        case 3:
-            tl3.reverse()
-            console.log(tl+" reversed")
-            break;
-        default:
-            tl1.reverse()
-            tl2.reverse()
-            tl3.reverse()
-            console.log("all reversed")
-            break;
-    }
-}
+function reverseTimelines(b) {
 
+    tl1.timeScale(2).reverse()
+    tl2.timeScale(2).reverse()
+    tl3.timeScale(2).reverse()
+    console.log("all reversed") 
+}
 
 
 function cardSelect(b){
@@ -98,3 +101,4 @@ function cardSelect(b){
     isActivated = !isActivated;
     
 }
+
