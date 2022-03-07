76f04ee4d2bd66b924fe1eb5556b35cee51d1c5c
therealbeez <gabriel.agbese2@gmail.com>
2022-03-07 23:54:28 +0100

    stable animation (for most probable use cases)

--- src/components/cardcontainer.js
+++ src/components/cardcontainer.js
@@ -9,14 +9,17 @@ export default function CardContainer(params) {
     return(
         <div className="card-container">
             
-            <div class="card" id="card-3" onClick={() => {cardSelect(3)}}>
-                card 3
+            <div className="card" id="card-3" >
+                <div onClick={() => {cardSelect(3)}}>card 3</div>
+                <p class="close" onClick={() => reverseTimelines(3)}>XX</p>
             </div>
-            <div class="card" id="card-2" onClick={() => {cardSelect(2)}}>
-                card 2
+            <div className="card" id="card-2">
+            <div onClick={() => {cardSelect(2)}}>card 2</div>
+                <p class="close" onClick={() => reverseTimelines(2)}>XX</p>
             </div>
-            <div class="card" id="card-1" onClick={() => {cardSelect(1)}}>
-                card 1
+            <div className="card" id="card-1">
+            <div onClick={() => {cardSelect(1)}}>card 1</div>
+                <p class="close" onClick={() => reverseTimelines(1)}>XX</p>
             </div>
         </div>
     )
@@ -36,6 +39,7 @@ var tl3 =  gsap.timeline();
 
 //card animation instructions
 const ani = (a) => {
+    console.log( a + "Activated")
 
     if(window.innerWidth >= 720){
         //desktop screens
@@ -75,30 +79,55 @@ const ani = (a) => {
                 tl2.to("#card-1", {y: 10+'vh',}).timeScale(1.0)
                 return tl2
             case 3:
+                tl3.to("#card-3", { y: -40+'vh',});
                 tl3.to("#card-2", { y:10+'vh',});
                 tl3.to("#card-1", {y: 10+'vh',}).timeScale(1.0)
-                tl3.to("#card-3", { y: -40+'vh',});
                 return tl3
             default:
                 break;
         }
 
     }
+    
 };
 
 //reversal of card activation animation 
-function reverseTimelines(b) {
-
-    tl1.timeScale(2).reverse()
-    tl2.timeScale(2).reverse()
-    tl3.timeScale(2).reverse()
-    console.log("all reversed") 
+function reverseTimelines(a) {
+    switch (a) {
+        case 1:
+            tl1.timeScale(2).reverse()
+            break;
+        case 2:
+            tl2.timeScale(2).reverse()
+            break;
+        case 3:
+            tl3.timeScale(2).reverse()
+            break;
+        default:
+            tl3.timeScale(2).reverse()
+            tl2.timeScale(2).reverse()
+            tl1.timeScale(2).reverse()
+            break;
+    }
+    console.log(a) 
 }
 
+let oldVal;
 
 function cardSelect(b){
-    isActivated ? reverseTimelines() : ani(b).play();
-    isActivated = !isActivated;
+    if (oldVal != null && (b != oldVal && isActivated == true)){
+        //at the time of pause, it was discovered that thhe reverse below is logging reverse, but not actually reversing, 
+        //"fired is also being logged, so it does reach"
+        //reverseTimelines("fired1");
+        console.log("fired2")
+        ani(b).play()
+        return
+        
+    }else{
+        ani(b).play();
+        isActivated = !isActivated;
+    }
+    oldVal = b;
     
 }
 
